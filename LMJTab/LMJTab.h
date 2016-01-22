95c58b6019038ba3529100c01e612022f399a7b6
limingjie <limingjie_mail@163.com>
2016-01-22 11:00:59 +0800

    1.0.0

--- LMJTab/LMJTab.h
+++ LMJTab/LMJTab.h
@@ -2,8 +2,14 @@
 //  LMJTab.h
 //  选项卡
 //
-//  Created by apple on 14-12-15.
-//  Copyright (c) 2014年 杰睿科技. All rights reserved.
+//  Version:1.0.0
+//
+//  Created by MajorLi on 14-12-15.
+//  Copyright (c) 2014年 iOS开发者公会. All rights reserved.
+//
+//  iOS开发者公会-技术1群 QQ群号：87440292
+//  iOS开发者公会-技术2群 QQ群号：232702419
+//  iOS开发者公会-议事区  QQ群号：413102158
 //
 
 #import <UIKit/UIKit.h>
