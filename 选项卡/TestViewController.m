65b4984c29ccd8b3254a1bc44197810fd4a49183
li.mingjie <li.mingjie@artpollo.com>
2016-08-25 10:34:57 +0800

    1.0.0

--- 选项卡/TestViewController.m
+++ 选项卡/TestViewController.m
@@ -24,8 +24,8 @@
     
 
     LMJTab * tab = [[LMJTab alloc] initWithFrame:CGRectMake(10, 50, 300, 30) lineWidth:1 lineColor:[UIColor blackColor]];
-    [tab setItemsWithTitle:[NSArray arrayWithObjects:@"小贴士",@"知识",@"精华帖", nil] normalItemColor:[UIColor whiteColor] selectItemColor:[UIColor blackColor] normalTitleColor:[UIColor blackColor] selectTitleColor:[UIColor whiteColor] titleTextSize:15 selectItemNumber:1];
-    tab.delegate = self;
+    [tab setItemsWithTitle:[NSArray arrayWithObjects:@"选项一",@"选项二",@"选项三", nil] normalItemColor:[UIColor whiteColor] selectItemColor:[UIColor blackColor] normalTitleColor:[UIColor blackColor] selectTitleColor:[UIColor whiteColor] titleTextSize:15 selectItemNumber:1];
+    tab.delegate           = self;
     tab.layer.cornerRadius = 5.0;
     [self.view addSubview:tab];
 
